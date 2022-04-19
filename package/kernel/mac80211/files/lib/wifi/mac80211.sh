112cec2e203aec11d2e6a8721858321b8bc6c259
only-wl <63798165+only-wl@users.noreply.github.com>
2022-04-19 19:57:21 +0800

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -167,6 +167,12 @@ detect_mac80211() {
 			dev_id="set wireless.radio${devidx}.macaddr=$(cat /sys/class/ieee80211/${dev}/macaddress)"
 		fi
 
+		if [ x$mode_band == x"5g" ]; then
+			ssid_5ghz="_5GHz"
+		else
+			ssid_5ghz="_2.4GHz"
+		fi
+
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
 			set wireless.radio${devidx}.type=mac80211
@@ -174,14 +180,15 @@ detect_mac80211() {
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
+			set wireless.radio${devidx}.country=CN
 			set wireless.radio${devidx}.disabled=0
-			set wireless.radio${devidx}.country=US
-
+			set wireless.radio${devidx}.noscan=1
+			set wireless.radio${devidx}.cell_density=0
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
+			set wireless.default_radio${devidx}.ssid=HiWiFi${ssid_5ghz}_$(cat /sys/class/ieee80211/${dev}/macaddress | awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 			set wireless.default_radio${devidx}.encryption=none
 EOF
 		uci -q commit wireless
